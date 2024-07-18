d864bca67174e094c7b7fc8247695d5a69a6bbea
aman3407 <aman.g@consultadd.com>
2024-07-18 08:48:01 +0530

    implemented error handling

--- app/db.py
+++ app/db.py
@@ -7,18 +7,24 @@ def getNewId():
 def connect():
     conn = sqlite3.connect('employees.db')
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS employees (id INTEGER PRIMARY KEY,name STRING, isActive BOOLEAN, dateOfJoining DATE)")
+    cur.execute("CREATE TABLE IF NOT EXISTS employees (id INTEGER , emp_id INTEGER PRIMARY KEY, name STRING, isActive BOOLEAN, dateOfJoining DATE, email STRING)")
     conn.commit()
     conn.close()
 
 def insert(employee):
     conn = sqlite3.connect('employees.db')
     cur = conn.cursor()
-    cur.execute("INSERT INTO employees VALUES (?,?,?,?)", (
+    print(f"Inserting employee: id={employee.id} (type={type(employee.id)}), emp_id={employee.emp_id} (type={type(employee.emp_id)}), "
+          f"name={employee.name} (type={type(employee.name)}), isActive={employee.isActive} (type={type(employee.isActive)}), "
+          f"dateOfJoining={employee.dateOfJoining} (type={type(employee.dateOfJoining)}), email={employee.email} (type={type(employee.email)})")
+
+    cur.execute("INSERT INTO employees (id, emp_id, name, isActive, dateOfJoining, email) VALUES (?, ?, ?, ?, ?, ?)", (
         employee.id,
+        employee.emp_id,
         employee.name,
         employee.isActive,
-        employee.dateOfJoining
+        employee.dateOfJoining,
+        employee.email
     ))
     conn.commit()
     conn.close()
@@ -30,7 +36,7 @@ def view():
     rows = cur.fetchall()
     employees = []
     for i in rows:
-        employee = Employee(i[0], i[1], i[2], i[3])
+        employee = Employee(i[0], i[1], i[2], i[3], i[4], i[5])
         employees.append(employee)
     conn.close()
     return employees
@@ -38,20 +44,19 @@ def view():
 def update(employee):
     conn = sqlite3.connect('employees.db')
     cur = conn.cursor()
-    cur.execute("UPDATE employees SET name=?, isActive=?, dateOfJoining=? WHERE id=?", (employee.name, employee.isActive,employee.dateOfJoining, employee.id))
+    cur.execute("UPDATE employees SET name=?, isActive=?, dateOfJoining=?, email=? WHERE emp_id=?", (
+        employee.name,
+        employee.isActive,
+        employee.dateOfJoining,
+        employee.email,
+        employee.emp_id
+    ))
     conn.commit()
     conn.close()
 
-def delete(id):
+def delete(emp_id):
     conn = sqlite3.connect('employees.db')
     cur = conn.cursor()
-    cur.execute("DELETE FROM employees WHERE id=?", (id,))
-    conn.commit()
-    conn.close()
-
-def deleteAll():
-    conn = sqlite3.connect('employees.db')
-    cur = conn.cursor()
-    cur.execute("DELETE FROM employees")
+    cur.execute("DELETE FROM employees WHERE emp_id=?", (emp_id,))
     conn.commit()
     conn.close()
